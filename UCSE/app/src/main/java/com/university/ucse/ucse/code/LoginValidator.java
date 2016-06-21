76403a7cb6a7187e319fb54f9f9e0bc81f27322a
Federico Pellegrinet <federico.pellegrinet@mercadolibre.com>
2016-06-20 23:01:26 -0300

    Add first version

---
+++ UCSE/app/src/main/java/com/university/ucse/ucse/code/LoginValidator.java
@@ -0,0 +1,7 @@
+package com.university.ucse.ucse.code;
+
+public class LoginValidator {
+    public static Boolean validate(String user, String password) {
+        return true;
+    }
+}
---
+++ UCSE/app/src/main/java/com/university/ucse/ucse/code/MainAdapter.java
@@ -0,0 +1,10 @@
+package com.university.ucse.ucse.code;
+
+import android.content.Context;
+
+public class MainAdapter {
+    public MainAdapter(Context context, String username) {
+
+    }
+
+}
